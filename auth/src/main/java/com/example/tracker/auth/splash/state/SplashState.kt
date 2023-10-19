ab9e38562afa90e318194e853e20e5748002b5d9
Misha Kamskov <143383894+Misha-Kamskov@users.noreply.github.com>
2023-10-19 18:26:17 +0300

    Delete auth directory

--- auth/src/main/java/com/example/tracker/auth/splash/state/SplashState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tracker.auth.splash.state
-
-import com.example.tracker.mvi.states.AbstractState
-import com.example.tracker.auth.splash.SplashContract
-
-class SplashState : AbstractState<SplashContract.View, SplashState>()
